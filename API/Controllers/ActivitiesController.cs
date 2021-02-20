b28ff53fe13095c170c0adb81bf5f0ab1f1a8f10
Hasbulla <hasbullakhan20@gmail.com>
2021-02-21 01:32:22 +0600

    Created Our Base App

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+using System;
+using Domain;
+using Persistence;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities() {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]        
+        public async Task<ActionResult<Activity>> GetActivity(Guid id) {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
